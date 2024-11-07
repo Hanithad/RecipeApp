2e7db36210fb2fe2365084959c02d1cf07c2f139
Hanitha <dhanitha92@gmail.com>
2024-11-07 16:42:18 -0600

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Hanitha Dhavileswarapu on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
